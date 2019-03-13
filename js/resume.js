38eb1682660dd5534844099b90e3a6090a3bd86e
srjamesjr <jamespoirierjr@hotmail.com>
2019-03-13 07:11:05 -0300

    Add files via upload

---
+++ js/resume.js
@@ -0,0 +1,8 @@
+function P_Resume() {
+    var d = document.getElementById("testid");
+    d.classList = "content";
+    d.innerHTML =
+        "<img src=\"../images/Resume.png\" alt=\"Resume (image not found)\">" +
+        "";
+
+}
