5ed624eacba4e6bfb9d6d511796f67935a638cc2
srjamesjr <jamespoirierjr@hotmail.com>
2019-04-24 03:01:25 -0300

    edited colours

--- js/software.js
+++ js/software.js
@@ -4,11 +4,11 @@ function P_BattleShip() {
     d.classList = "content";
     d.innerHTML =
         '<div class=\"center-text\"><h1>Python BattleShip</h1></div> \
-        <p>&emsp;&emsp;&emsp;&emsp;As an assignment in my first year, end of first semester; we had to create a CLI battleship type game. Ships would be randomly generated on a 10x8 grid and the user would choose a space to fire at. I wanted to go further then this.</p>\
+        <p>&emsp;&emsp;&emsp;&emsp;As an assignment in my first year, end of first semester; we had to create a CLI battleship type game. Ships would be randomly generated on a 10x8 grid and the user would choose a space to fire at. I wanted to go beyond the assignment.</p>\
         <button class="collapsible">The project: &#9660;</button> \
         <div class="section">  \
-        <p> this is a test my good sir, m\'lady (1) </p>\
-        \
+        <p>&emsp;&emsp;&emsp;&emsp;I created the classic game of battleship. Playable by 1 person and a Computer Player, or by 2 people.  </p>\
+        <p>&emsp;&emsp;&emsp;&emsp;You start the game with a .bat file that loads an ASCII menu asking to choose how many players are playing. This launches a Python Turtle graphics window (I explain this in the next section). At the start of the game you need to place your boats. You do this one by one starting at the smallest using the arrow keys to move the boat around the grid and the \'R\' button to rotate the ship (and space to place). When all Your boats are placed, they are hidden and it\'s the second players turn. </p>\
         </div>  <div><br/></div> \
         \
          <button class="collapsible">Obstacles: &#9660;</button>\
@@ -24,3 +24,5 @@ function P_BattleShip() {
         </div>  <div><br/></div>  \
         ';
 }
+
+//<p>&emsp;&emsp;&emsp;&emsp;</p>
