482b03906012b518fce3237bd39d5ade61f86d4a
james <w0287666@nscc.ca>
2019-05-21 05:35:07 -0300

    added Java project and Goals

--- js/resume.js
+++ js/resume.js
@@ -12,7 +12,7 @@ function P_Goals() {
     d.classList = "content";
     d.innerHTML =
         '<div class=\"center-text\"><h4>Short-term Goals</h4></div> \
-        <p>&emsp;&emsp;&emsp;&emsp;Learn Laravel: For my work term, I\'m product owner of a team, creating a website in Laravel. I\'d like to help with the coding, not just mechanics, direction, and github managing</p>\
+        <p>&emsp;&emsp;&emsp;&emsp;Learn Laravel: For my work term, I\'m product owner and leader of a team, creating a website in Laravel. I taught myself PHP last year, and Laravel builds off of that</p>\
         <p>&emsp;&emsp;&emsp;&emsp;Add more more examples of project to this website: I have about 4 more project in each category that I want to add when I get time</p>\
         <p>&emsp;&emsp;&emsp;&emsp;Update my linkedin: I\'d like to make my linkedin have relevant information, a good layout,my best work, and a link to this website </p>\
         <div class="center-text"><h4>Long-term Goals</h4></div> \
